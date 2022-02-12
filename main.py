57ffa1e96f67c250cf1637b7b1995dd417000dff
Artur <aat281205@gmail.com>
2022-02-12 13:49:02 +0300

    new_commit

--- main.py
+++ main.py
@@ -35,12 +35,41 @@ def show_png():
                   </head>
                   <body>
                   <h1> Жди нас, Марс! </h1>
-                    <img src="{url_for('static', filename='img/mars.png')}" 
+                    <img src="{url_for('static', filename='img/mars.png')}" >
                     <p></p>
-                    <p> марс пригоден для жизни </p>
+                    <p><strong> марс пригоден для жизни !!! </strong> </p>
                   </body>
                 </html>"""
 
 
+@app.route('/promotion_image')
+def bootstrap():
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <title>Колонизация</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='img/mars.png')}" >
+                    <p></p>
+                    <div class="alert alert-primary" role="alert">
+                      колонизация марса будет
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                      Марс достижимая планета
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                    земли марса будут покорены
+                    </div>
+                  </body>
+                </html>'''
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
