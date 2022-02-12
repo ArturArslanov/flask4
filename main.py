675d4d7ec9118ca1993c3c0d054d3e8f2e945b2d
Artur <aat281205@gmail.com>
2022-02-12 18:09:58 +0300

    new_commit

--- main.py
+++ main.py
@@ -284,6 +284,34 @@ def bootstrap():
                   </body>
                 </html>'''
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def show_results(nickname,level,rating):
+    return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                        <title>результаты</title>
+                      </head>
+                      <body>
+                        <h1 id=redh>результаты отбора</h1>
+                        <h1 id=bluep> претендент на участие {nickname} </h1>
+                        <div class="alert alert-warning" role="alert">
+                            ваш рэйтинг {level}
+                        </div>
+                        <div class="alert alert-info" role="alert">
+                            составляет {rating}
+                        </div>
+                        <div class="alert alert-dark" role="alert">
+                            желаем удачи
+                        </div>
+                      </body>
+                    </html>'''
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
