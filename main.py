b7b935d4138dec3533343a46ee4401cc46088473
Artur <aat281205@gmail.com>
2022-02-12 17:57:51 +0300

    new_commit

--- main.py
+++ main.py
@@ -46,7 +46,40 @@ def show_png():
 
 @app.route('/choice/<string:planet>')
 def any_(planet: str):
-    return f'<strong>{planet.capitalize()}</strong>'
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <title>Колонизация</title>
+                  </head>
+                  <body>
+                    <h1 id=redh>Жди нас, {planet}!</h1>
+                    <h1> {planet} не может остаться пустой планетой </h1>
+                    <p></p>
+                    <div class="alert alert-primary" role="alert">
+                      колонизация {planet} будет  
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                      {planet} наша вторая земля
+                    </div>
+                    <div class="alert alert-info" role="alert">
+                     сделаем {planet} пригодной для жизни
+                    </div>
+                    <div class="alert alert-light" role="alert">
+                      {planet} очень близок
+                    </div>
+                    <div class="alert alert-dark" role="alert">
+                      присоединяйся
+                    </div>
+                    </div>
+                  </body>
+                </html>'''
 
 
 @app.route('/form', methods=['POST', 'GET'])
